1e71a04b32211bb61f62311f356801ad428bcb7c
msatrioh <satrioharyo_r10@yahoo.co.id>
2025-03-06 13:59:57 +0700

    [RED] Add OrderServiceImpl skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -50,7 +50,7 @@ class OrderServiceImplTest {
         doReturn(order).when(orderRepository).save(order);
 
         Order result = orderService.createOrder(order);
-        verify(orderRepository).createOrder(order);
+        verify(orderRepository, times(1)).save(order);
         assertEquals(order.getId(), result.getId());
     }
 
@@ -65,21 +65,6 @@ class OrderServiceImplTest {
 
     @Test
     void testUpdateStatus() {
-        Order order = orders.get(1);
-        Order newOrder = new Order(order.getId(), order.getProducts(), order.getOrderTime(),
-                order.getAuthor(), OrderStatus.SUCCESS.getValue());
-        doReturn(order).when(orderRepository).findById(order.getId());
-        doReturn(newOrder).when(orderRepository).save(any(Order.class));
-
-        Order result = orderService.updateStatus(order.getId(), OrderStatus.SUCCESS.getValue());
-
-        assertEquals(order.getId(), result.getId());
-        assertEquals(OrderStatus.SUCCESS.getValue(), result.getStatus());
-        verify(orderRepository, times(1)).save(any(Order.class));
-    }
-
-    @Test
-    void testUpdateStatusInvalidStatus() {
         Order order = orders.get(1);
         Order newOrder = new Order(order.getId(), order.getProducts(), order.getOrderTime(),
                 order.getAuthor(), OrderStatus.SUCCESS.getValue());
